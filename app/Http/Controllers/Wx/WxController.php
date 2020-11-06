aa3314f5bb338d4fe6a686dd4e07033582f25779
Your Name <2307834325@qq.com>
2020-11-06 19:20:00 +0800

    weixin

--- app/Http/Controllers/Wx/WxController.php
+++ app/Http/Controllers/Wx/WxController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Wx;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class WxController extends Controller
 {
@@ -11,13 +12,17 @@ class WxController extends Controller
         $res = request()->get('echostr','');
         if($this->checkSignature() && !empty($res)){
             echo $res;
+        }else{
+            $res = $this->getAccesstoken();
+            dd($res);
         }
+
     }
     private function checkSignature()
 {
-    $signature = $_GET["signature"];
-    $timestamp = $_GET["timestamp"];
-    $nonce = $_GET["nonce"];
+    $signature = request()->get("signature");
+    $timestamp = request()->get("timestamp");
+    $nonce = request()->get("nonce");
 	
     $token = "test";
     $tmpArr = array($token, $timestamp, $nonce);
@@ -31,4 +36,17 @@ class WxController extends Controller
         return false;
     }
 }
+    public function getAccesstoken(){
+        $key = 'weiAccess_token';
+        if(!Redis::get($key)){
+            $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx8b14088e2da627cf&secret=10a2cada2130e0ada2e6e476ad16d448";
+            $token = file_get_contents($url);
+            dd($token);
+//            $token = json_decode($token,true);
+
+//            if(){
+//
+//            }
+        }
+    }
 }
